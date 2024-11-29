d2e42edc48726d953a02487670cd34bd8c9d84ae
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2024-11-29 11:07:22 +0500

    fix issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-
 import {
   CredentialsSignInButton,
   GithubSignInButton,
@@ -9,23 +8,28 @@ import { authConfig } from "@/lib/auth";
 import { redirect } from "next/navigation";
 
 export default async function SignInPage() {
+  // Fetch the server session
   const session = await getServerSession(authConfig);
 
+  // Debugging log (server-side only)
   console.log("Session: ", session);
 
-  if (session) return redirect("/timeline");
+  // Redirect if session exists
+  if (session) {
+    redirect("/timeline");
+  }
 
+  // Render sign-in UI for users without a session
   return (
     <div className="w-full flex flex-col items-center justify-center min-h-screen py-2 bg-white">
       <div className="flex flex-col items-center mt-10 p-10 shadow-2xl">
         <h1 className="mt-10 mb-4 text-4xl font-bold">Sign In</h1>
         <GoogleSignInButton />
         <GithubSignInButton />
-        <span className="text-2xl font-semibold text-center mt-8">
-          Or
-        </span>
+        <span className="text-2xl font-semibold text-center mt-8">Or</span>
         <CredentialsSignInButton />
       </div>
     </div>
   );
 }
+
--- components/authButtons.tsx
+++ components/authButtons.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Image from 'next/image';
 
 import githubLogo from "@/public/github.png";
 import { useRouter } from 'next/navigation';
@@ -14,7 +15,12 @@ export function GoogleSignInButton() {
       onClick={handleClick}
       className="w-full flex items-center font-semibold justify-center h-14 px-6 mt-4 text-xl  transition-colors duration-300 bg-white border-2 border-black text-black rounded-lg focus:shadow-outline hover:bg-slate-200"
     >
-      <img src='/google.png' width={20} height={20}/>
+      <Image
+        src="/google.png"
+        alt="Description of the image"
+        width={20}
+        height={20}
+      />
       <span className="ml-4">Continue with Google</span>
     </button>
   );
@@ -30,7 +36,12 @@ export function GithubSignInButton() {
       onClick={handleClick}
       className="w-full flex items-center font-semibold justify-center h-14 px-6 mt-4 text-xl transition-colors duration-300 bg-white border-2 border-black text-black rounded-lg focus:shadow-outline hover:bg-slate-200"
     >
-      <img src="/github.png" width={20} height={20}  />
+      <Image
+        src="/github.png"
+        alt="Description of the image"
+        width={20}
+        height={20}
+      />
       <span className="ml-4">Continue with Github</span>
     </button>
   );
